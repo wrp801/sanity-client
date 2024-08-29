43ec9c6140d075231d8cc2a742e373d02d5619dd
wrp801 <wespoulsen@gmail.com>
2024-08-29 11:38:44 -0600

    added export tests

--- tests/test_export.rs
+++ tests/test_export.rs
@@ -2,6 +2,8 @@ use sanity_client::sanity::client::SanityClient;
 use dotenv::dotenv;
 use std::env;
 use serde_json::json;
+use std::path::PathBuf;
+use std::fs;
 
 fn setup() -> SanityClient {
     dotenv().ok();
@@ -11,22 +13,27 @@ fn setup() -> SanityClient {
     SanityClient::new(token, dataset, project)
 }
 
-
 #[tokio::test] 
 async fn test_successful_export_no_write() {
     let client = setup();
 
     let doc_types = vec!(String::from("blueprints"));
+    let path = PathBuf::from("tests/export_output.ndjson");
 
-    let res = client 
-        .export() 
+    let _ = client 
+        .export()
         .doc_type(doc_types)
         .fetch()
-        .await;
+        .await 
+        .unwrap() 
+        .write(path.clone());
 
-    assert!(res.is_ok())
+    assert!(path.exists());
 
+    let res = fs::remove_file(path);
+    assert!(res.is_ok());
 }
+
 
 
 
