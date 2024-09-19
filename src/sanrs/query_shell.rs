9836ececedeedc3b10b5ed2518f56da263e6a1a3
wrp801 <wespoulsen@gmail.com>
2024-09-19 17:32:44 -0600

    removed debug representation of query in interactive mode

--- src/sanrs/query_shell.rs
+++ src/sanrs/query_shell.rs
@@ -28,10 +28,8 @@ pub async fn run_shell(client: &SanityClient) {
         let response = client.query().fetch(input_str).await;
         match response {
             Ok(resp) => {
-                // FIX: This is not printing to a pretty json string. Goal is to have it print to a
-                // human friendly format
                 let pretty_string = serde_json::to_string_pretty(&resp.result).unwrap();
-                println!("{:?}", pretty_string);
+                println!("{}", pretty_string);
                 input.clear()
             }
             Err(err) => {
